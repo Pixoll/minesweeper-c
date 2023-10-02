2545752fac1c1e9385ffac2575148e80477f56d7
Pixoll <pixoll.gamer@gmail.com>
2023-10-02 17:07:32 -0300

    Add SDL_image parameter

--- build.sh
+++ build.sh
@@ -12,6 +12,6 @@ for file in `find ./src -type f -name "*.c"`; do
     files="$files \"$file\"" 
 done
 
-command="gcc -g $files -o $exename.exe -lSDL2 -lSDL2_ttf"
+command="gcc -g $files -o $exename.exe -lSDL2 -lSDL2_ttf -lSDL2_image"
 echo Running: $command
 eval $command
