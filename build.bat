41ecfafd35ef7d13a4948357ee3a5d05a1d5ae22
Pixoll <pixoll.gamer@gmail.com>
2023-10-04 00:06:07 -0300

    Colored compiling

--- build.bat
+++ build.bat
@@ -9,6 +9,6 @@ for /f "tokens=*" %%f in ('dir src\*.c /s/b') do (
     set files=!files! "%%f"
 )
 
-set command=gcc !files! -I .\include -L .\lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -lSDL2_image -o "!exename!.exe"
+set command=gcc -fdiagnostics-color=always !files! -I .\include -L .\lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -lSDL2_image -o "!exename!.exe"
 echo Running: %command%
 %command%
