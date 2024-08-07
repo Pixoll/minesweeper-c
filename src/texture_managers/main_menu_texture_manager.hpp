d0a3d9fdb8ac2072954c950ddc2ea23f2e555740
Pixoll <pixoll.gamer@gmail.com>
2024-08-07 14:28:24 -0400

    Set parameters as members

--- src/texture_managers/main_menu_texture_manager.hpp
+++ src/texture_managers/main_menu_texture_manager.hpp
@@ -22,8 +22,13 @@ private:
     static constexpr auto BIG_MINE_IMAGE_PATH = "assets/textures/mine_big.png";
     static constexpr auto GAME_BUTTON_IMAGE_PATH = "assets/textures/button_game.png";
 
+    SDL_Renderer *m_renderer;
+    const int m_window_width;
+    const int m_window_height;
+
     MainMenuTexture m_big_mine_texture;
     MainMenuTexture m_title_texture;
+
     MainMenuTexture m_new_game_button_texture;
     MainMenuTexture m_continue_game_button_texture;
 
@@ -31,14 +36,17 @@ private:
 
 public:
     MainMenuTextureManager(SDL_Renderer *renderer, const int window_width, const int window_height) :
-        m_title_font(Font::RUBIK_LIGHT, window_height * 0.04) {
-        init_big_mine_texture(renderer, window_width, window_height);
-        init_title_texture(renderer, window_width);
+        m_renderer(renderer),
+        m_window_width(window_width),
+        m_window_height(window_height),
+        m_title_font(Font::RUBIK_LIGHT, m_window_height * 0.04) {
+        init_big_mine_texture();
+        init_title_texture();
 
         const int width = m_big_mine_texture->get_w() * 1.86f;
         const int x = m_big_mine_texture->get_x() + (m_big_mine_texture->get_w() - width) / 2;
-        init_new_game_button(renderer, window_height, width, x);
-        init_continute_game_button(renderer, width, x);
+        init_new_game_button(width, x);
+        init_continute_game_button(width, x);
     }
 
     ~MainMenuTextureManager() = default;
@@ -54,13 +62,13 @@ public:
     }
 
 private:
-    void init_big_mine_texture(SDL_Renderer *renderer, const int window_width, const int window_height) {
-        const int y = window_height * 0.1;
-        const int size = window_height * 0.25;
-        const int x = (window_width - size) / 2;
+    void init_big_mine_texture() {
+        const int y = m_window_height * 0.1;
+        const int size = m_window_height * 0.25;
+        const int x = (m_window_width - size) / 2;
 
         m_big_mine_texture = std::make_shared<Texture>(
-            renderer,
+            m_renderer,
             BIG_MINE_IMAGE_PATH,
             SDL_Rect{x, y, size, size}
         );
@@ -68,24 +76,24 @@ private:
         m_big_mine_texture->set_color_mod(Color::THEME);
     }
 
-    void init_title_texture(SDL_Renderer *renderer, const int window_width) {
-        m_title_texture = std::make_shared<Texture>(renderer, m_title_font.get_font(), "Minesweeper", Color::WHITE);
+    void init_title_texture() {
+        m_title_texture = std::make_shared<Texture>(m_renderer, m_title_font.get_font(), "Minesweeper", Color::WHITE);
         m_title_texture->set_position(
-            (window_width - m_title_texture->get_w()) / 2,
+            (m_window_width - m_title_texture->get_w()) / 2,
             static_cast<int>((m_big_mine_texture->get_y() + m_big_mine_texture->get_h()) * 1.1)
         );
     }
 
-    void init_new_game_button(SDL_Renderer *renderer, const int window_height, const int width, const int x) {
-        Texture button_texture(renderer, GAME_BUTTON_IMAGE_PATH);
+    void init_new_game_button(const int width, const int x) {
+        Texture button_texture(m_renderer, GAME_BUTTON_IMAGE_PATH);
         button_texture.set_color_mod(Color::LIGHTER_GREY);
         button_texture.set_width(width);
 
         m_new_game_button_texture = std::make_shared<Texture>(
-            renderer,
+            m_renderer,
             SDL_Rect{
                 x,
-                window_height / 2 + static_cast<int>(button_texture.get_h() * 1.5),
+                m_window_height / 2 + static_cast<int>(button_texture.get_h() * 1.5),
                 width,
                 button_texture.get_h(),
             }
@@ -94,7 +102,7 @@ private:
 
         button_texture.render();
 
-        Texture text_texture(renderer, Font::get_shared(Font::PRIMARY)->get_font(), "New game", Color::WHITE);
+        Texture text_texture(m_renderer, Font::get_shared(Font::PRIMARY)->get_font(), "New game", Color::WHITE);
         text_texture.set_position(
             (button_texture.get_w() - text_texture.get_w()) / 2,
             (button_texture.get_h() - text_texture.get_h()) / 2
@@ -102,13 +110,13 @@ private:
         text_texture.render();
     }
 
-    void init_continute_game_button(SDL_Renderer *renderer, const int width, const int x) {
-        Texture button_texture(renderer, GAME_BUTTON_IMAGE_PATH);
+    void init_continute_game_button(const int width, const int x) {
+        Texture button_texture(m_renderer, GAME_BUTTON_IMAGE_PATH);
         button_texture.set_color_mod(Color::LIGHTER_GREY);
         button_texture.set_width(width);
 
         m_continue_game_button_texture = std::make_shared<Texture>(
-            renderer,
+            m_renderer,
             SDL_Rect{
                 x,
                 static_cast<int>(m_new_game_button_texture->get_y() + m_new_game_button_texture->get_h() * 1.5),
@@ -120,7 +128,7 @@ private:
 
         button_texture.render();
 
-        Texture text_texture(renderer, Font::get_shared(Font::PRIMARY)->get_font(), "Continue", Color::WHITE);
+        Texture text_texture(m_renderer, Font::get_shared(Font::PRIMARY)->get_font(), "Continue", Color::WHITE);
         text_texture.set_position(
             (button_texture.get_w() - text_texture.get_w()) / 2,
             (button_texture.get_h() - text_texture.get_h()) / 2
